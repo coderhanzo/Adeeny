2121b9ee58a02e213aabcd04439c408b194b8730
coderhanzo <reinerbielert@icloud.com>
2024-08-07 09:49:15 +0000

    refixed custom permssions

--- apps/donation_management/views.py
+++ apps/donation_management/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import authentication, permissions
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdmin, IsAuthenticated
 from rest_framework.decorators import (
     api_view,
     permission_classes,
@@ -53,7 +53,7 @@ class GetAllWaqfDonations(APIView):
         alldonations = ProjectDonation.objects.all()
         serializer = WaqfDonationsSerializer(alldonations, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
 # create waqf donations
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
@@ -67,5 +67,3 @@ def create_waqf_donation(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    
--- apps/users/custom_permissions.py
+++ apps/users/custom_permissions.py
@@ -1,34 +1,21 @@
-# Extending default Permissions
 from rest_framework import permissions
+from .models import User
 
-
-class IsAdminUser(permissions.BasePermission):
+class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        if not user:
-            return False
-        return getattr(user, "is_admin", False) or getattr(user, "is_imam", False)
+        return request.user.role in [User.Role.ADMIN, User.Role.IMAM]
 
 
 class IsSuperAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        if not user:
-            return False
-        return getattr(user, "is_superadmin", False)
+        return request.user.role == User.Role.SUPERADMIN
 
 
 class IsImam(permissions.BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        if not user:
-            return False
-        return getattr(user, "is_imam", False)
+        return request.user.role == User.Role.IMAM
 
 
 class IsAssociate(permissions.BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        if not user:
-            return False
-        return getattr(user, "is_associate", False)
+        return request.user.role == User.Role.ASSOCIATE
