23020b7372935dc5e0ceaae762dccb9e132fa545
coderhanzo <reinerbielert@icloud.com>
2024-09-24 13:44:36 +0000

    peoples pay is not integrating well, im not sure why. but user verification works alright

--- apps/transactions/services.py
+++ apps/transactions/services.py
@@ -1,4 +1,6 @@
+import os
 import requests
+from django.conf import settings
 
 
 # i am thinking that before any requests are made a token needs to be generated
@@ -6,25 +8,31 @@ import requests
 class PeoplesPayService:
     BASE_URL = "https://peoplespay.com.gh/peoplepay/hub"
 
-    # @staticmethod
-    # def get_token(merchantid, apikey):
-    #     url = f"{PeoplesPayService.BASE_URL}/token/get"
-    #     payload = {
-    #         "merchantid": merchantid,
-    #         "apikey": apikey,
-    #     }
-    #     headers = {"Content-Type": "application/json"}
-    #     try:
-    #         response = requests.post(url, json=payload, headers=headers, timeout=10)
-    #         response_data = response.json()
+    @staticmethod
+    def get_token():
+        merchantId = os.getenv("PEOPLES_PAY_MERCHANT_ID")
+        apikey = os.getenv("PEOPLES_PAY_API_KEY")
 
-    #         if response.status_code == 200 and "data" in response_data:
-    #             return response_data
-    #         else:
-    #             return None
-    #     except requests.exceptions.RequestException as e:
-    #         print(f"Error retrieving token: {e}")
-    #         return None
+        if not merchantId or not apikey:
+            raise ValueError("Merchant ID or API key not set in environment variables.")
+
+        url = f"{PeoplesPayService.BASE_URL}/token/get"
+        payload = {
+            "merchantId": merchantId,
+            "apikey": apikey,
+        }
+        headers = {"Content-Type": "application/json"}
+        try:
+            response = requests.post(url, json=payload, headers=headers, timeout=10)
+            response_data = response.json()
+
+            if response.status_code == 200 and "data" in response_data:
+                return response_data
+            else:
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f"Error retrieving token: {e}")
+            return None
 
     @staticmethod
     def disburse_money(
@@ -67,7 +75,7 @@ class PeoplesPayService:
             "account_number": account_number,
             "account_name": account_name,
             "account_issuer": account_issuer,
-            "callback_url": callback_url,
+            "callbackUrl": callback_url,
         }
         headers = {
             "Content-Type": "application/json",
--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -7,101 +7,127 @@ from .serializers import PaymentsSerializer, CollectionsSerializer
 from .services import PeoplesPayService
 import requests
 
+
 class PaymentsView(APIView):
     def post(self, request):
         payment_serializer = PaymentsSerializer(data=request.data)
 
+        # Get the token using the PeoplesPayService from the .get_token() method
+        token = PeoplesPayService.get_token()
+        print(token)
+        if token is None:
+            return Response(
+                {"message": "Failed to retrieve token"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         if payment_serializer.is_valid():
             validated_data = payment_serializer.validated_data
+        # Disburse payment
+        disburse_payload = {
+            "amount": str(validated_data["amount"]),
+            "account_number": validated_data["account_number"],
+            "account_name": validated_data["account_name"],
+            "account_issuer": validated_data["account_issuer"],
+            # "external_transaction_id": validated_data["external_transaction_id"],
+            "description": "Payment description",
+        }
+        disburse_headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token['data']}",
+        }
 
-            # we generate the token here
-            url = "https://peoplespay.com.gh/peoplepay/hub/token/get"
-            payload = {
-                "merchant_id": "66e058b1a69d30672127d1b6",
-                "api_key": "6dfbecfd-d091-40ff-9ed1-2e8324d12a35",
-            }
-            headers = {"Content-Type": "application/json"}
+        try:
+            disburse_response = requests.post(
+                f"{PeoplesPayService.BASE_URL}/disburse",
+                json=disburse_payload,
+                headers=disburse_headers,
+            )
+            print(disburse_headers)
+            disburse_data = disburse_response.json()
 
-            try:
-                token_response = requests.post(
-                    url, json=payload, headers=headers, timeout=10
-                )
-                token_data = token_response.json()
+            print(disburse_data)
 
-                if token_response.status_code == 200 and "data" in token_data:
-                    token = token_data["data"]
-                else:
-                    return Response(
-                        {"message": "Failed to retrieve token"},
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
-            except requests.exceptions.RequestException as e:
+            if disburse_response.status_code == 200 and disburse_data.get("success"):
+                payment_serializer.save()  # Save payment record to the database
                 return Response(
-                    {"message": f"Error retrieving token: {str(e)}"},
+                    {"message": "Payment processed successfully"},
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return Response(
+                    {"message": disburse_data.get("message", "Payment failed")},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
 
-            disburse_url = "https://peoplespay.com.gh/peoplepay/hub/disburse"
-            disburse_payload = {
-                "amount": str(validated_data["amount"]),
-                "account_number": validated_data["account_number"],
-                "account_name": validated_data["account_name"],
-                "account_issuer": validated_data["account_issuer"],
-                "external_transaction_id": validated_data["external_transaction_id"],
-            }
-            disburse_headers = {
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {token}",
-            }
-
-            try:
-                disburse_response = requests.post(
-                    disburse_url, json=disburse_payload, headers=disburse_headers
-                )
-                disburse_data = disburse_response.json()
-
-                # Check if disbursement was successful
-                if disburse_response.status_code == 200 and disburse_data.get(
-                    "success"
-                ):
-                    payment_serializer.save()  # Save the payment record to the database
-                    return Response(
-                        {"message": "Payment processed successfully"},
-                        status=status.HTTP_201_CREATED,
-                    )
-                else:
-                    return Response(
-                        {"message": disburse_data.get("message", "Payment failed")},
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
-
-            except requests.exceptions.RequestException as e:
-                return Response(
-                    {"message": f"Error processing payment: {str(e)}"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-
-        return Response(payment_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except requests.exceptions.RequestException as e:
+            return Response(
+                {"message": f"Error processing payment: {str(e)}"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
 class CollectionsView(APIView):
     def post(self, request):
         collection_serializer = CollectionsSerializer(data=request.data)
+
         if collection_serializer.is_valid():
-            # Use PeoplesPayService to process the collection
-            response = PeoplesPayService.process_collection(
-                collection_serializer.validated_data
-            )
-            if response["success"]:
-                collection_serializer.save()  # Save collection record in database
+            validated_data = collection_serializer.validated_data
+
+            # Get the token using the PeoplesPayService
+            token = PeoplesPayService.get_token()
+            print(f"second token: {token}")
+            if token is None:
                 return Response(
-                    {"message": "Collection processed successfully"},
-                    status=status.HTTP_201_CREATED,
+                    {"message": "Failed to retrieve token"},
+                    status=status.HTTP_400_BAD_REQUEST,
                 )
-            else:
+
+            # Process the collection
+            collection_payload = {
+                "amount": str(validated_data["amount"]),
+                "account_number": validated_data["account_number"],
+                "account_name": validated_data["account_name"],
+                "account_issuer": validated_data["account_issuer"],
+                "callbackUrl": validated_data["callback_url"],
+                "description": "Collection description",
+            }
+            collection_headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {token['data']}",
+            }
+
+            try:
+                collection_response = requests.post(
+                    f"{PeoplesPayService.BASE_URL}/collectmoney",
+                    json=collection_payload,
+                    headers=collection_headers,
+                )
+                collection_data = collection_response.json()
+
+                if collection_response.status_code == 200 and collection_data.get(
+                    "success"
+                ):
+                    collection_serializer.save()  # Save collection record to the database
+                    return Response(
+                        {"message": "Collection processed successfully"},
+                        status=status.HTTP_201_CREATED,
+                    )
+                else:
+                    return Response(
+                        {
+                            "message": collection_data.get(
+                                "message", "Collection failed"
+                            )
+                        },
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+
+            except requests.exceptions.RequestException as e:
                 return Response(
-                    {"message": response["message"]}, status=status.HTTP_400_BAD_REQUEST
+                    {"message": f"Error processing collection: {str(e)}"},
+                    status=status.HTTP_400_BAD_REQUEST,
                 )
+
         return Response(
             collection_serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
@@ -112,7 +138,7 @@ class PaymentCallbackAPIView(APIView):
         transaction_id = request.data.get("transactionId")
         status = request.data.get("success")
 
-        # Find the collection related to this transaction
+        # Find the collection related to this transaction using external_transaction_id
         try:
             collection = Collections.objects.get(external_transaction_id=transaction_id)
             if status == "true":
@@ -120,8 +146,10 @@ class PaymentCallbackAPIView(APIView):
             else:
                 collection.transaction_status = "failed"
             collection.save()
+
             return Response(
-                {"message": "Callback processed"}, status=status.HTTP_200_OK
+                {"message": "Callback processed successfully"},
+                status=status.HTTP_200_OK,
             )
         except Collections.DoesNotExist:
             return Response(
